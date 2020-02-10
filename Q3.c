4871124f1a7b4f5cdf0d6bd5218db491aef28c1d
SyedaKissa <60823225+SyedaKissa@users.noreply.github.com>
2020-02-11 01:36:07 +0500

    Update Q3.c

--- Q3.c
+++ Q3.c
@@ -4,58 +4,60 @@
 
 int main()
 {
-	FIlE *input,*output;
+	FILE *input,*output;
 	input  = fopen("a.txt", "r");
 	output = fopen("b.txt", "w");
 
-	char buffer[50];
-	char reverse[50];
+	char buffer[25];
+	char reverse[25];
 
     //Cleaning the Buffers        
     memset(buffer,'\0',sizeof(buffer));
     memset(reverse,'\0',sizeof(reverse));
 
-	//fscanf(input,"%s",buffer);
 	int i,j,length,flag;
 	flag = 0; // means no vowel
 
 	while( fscanf(input,"%s",buffer) != EOF )
 	{
-		for (i=0; i<50; i++)
+		printf("buffer: %s\n ", buffer);
+		for (i=0; i<25; i++)
 		{
+			printf("character : %c\n ", buffer[i]);
+			
 			if ( buffer[i] == 'a' || buffer[i] == 'e' || buffer[i] == 'i' || buffer[i] == 'o' || buffer[i] == 'u' || 
-			     buffer[i] == 'A' || buffer[i] == 'E' || buffer[i] == 'I' || buffer[i] == 'O' || buffer[i] == 'U')
-			{
+			     buffer[i] == 'A' || buffer[i] == 'E' || buffer[i] == 'I' || buffer[i] == 'O' || buffer[i] == 'U'){
+				
+				printf("character inside if : %c\n ", buffer[i]);
 				flag = 1;
 				break;
 			}
 		}
 
-		if (flag == 0)
-		{
-			// print the exact the same
-			fputs(buffer, output);
+		length = strlen(buffer); // strlen() function doesn't count the null character \0 while calculating the length
+ 
+		if (flag == 1) // have vowels so print same
+		{	
+		    flag = 0;		
+            buffer[length] = ' ';
+			fputs(buffer, output);			
 		}
-		else if (flag == 1)
-		{
-			flag = 0;
-			// print in reverse
-			length = strlen(buffer); // strlen() function doesn't count the null character \0 while calculating the length
-            i = 0;
+		else if (flag == 0) // have no vowels so print in reverse
+		{					
+			i = 0;
 			for (j=length-1; j>=0; j--)
 			{
 				reverse[j] = buffer[i];
 				i++;
 			}
-			reverse[length] = '\0';
-			fputs(reverse, output);		
+			reverse[length] = ' ';
+			reverse[length+1] = '\0';
+			fputs(reverse, output);					
 		}
 
 		//Cleaning the Buffers        
         memset(buffer,'\0',sizeof(buffer));
         memset(reverse,'\0',sizeof(reverse));
-
-        //fscanf(input,"%s",buffer);
 	}
 
 	fclose(input);
