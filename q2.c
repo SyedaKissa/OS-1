e93fc491530d908e6af250445ab38df31f50ae38
SyedaKissa <60823225+SyedaKissa@users.noreply.github.com>
2020-02-11 03:02:18 +0500

    Update q2.c

--- q2.c
+++ q2.c
@@ -6,8 +6,11 @@ int main()
 	FILE *output;	
 	output = fopen("b.txt", "w");
 
-	char c[2];
 	int option = 0;
+
+	char Name[50];
+	char Rollnumber[9];
+	char email[50];
 
 while (option == 4) 
 {
@@ -15,17 +18,23 @@ while (option == 4)
 	printf("%s\n", "2) Read student record ");
 	printf("%s\n", "3) Delete student record");
 	printf("%s\n", "4) Exit");
-	printf("%s\n", "Please select one of the options as (1 or 2 or 3 or 4)" );
+	printf("%s\n", "Please select one of the options as (1 or 2 or 3 or 4)");
 
 	scanf("%d",&option); // & is added with scanf
 
-	if (option == 4)
-	{
-		break;
-	}
+	if (option == 4) { break; }
 	else if (option == 1)
 	{
+		printf("%s\n", "Please type in Name of the student");
+		scanf("%s",&Name); // & is added with scanf
+		printf("%s\n", "Please type in Roll Number of the student");
+		scanf("%s",&Rollnumber); // & is added with scanf
+		printf("%s\n", "Please type in Email of the student");
+		scanf("%s",&email); // & is added with scanf
 
+		printf("%s\n", Name);
+		printf("%s\n", Rollnumber);
+		printf("%s\n", email);
 	}
 	else if (option == 2)
 	{
@@ -37,7 +46,7 @@ while (option == 4)
 	}
 	else 
 	{
-
+		printf("%s\n", "Please type in the correct option");
 	}
 	option = 0;	
 }
