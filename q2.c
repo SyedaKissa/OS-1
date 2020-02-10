584675539e0905e307a3a4c537d502f504697d80
SyedaKissa <60823225+SyedaKissa@users.noreply.github.com>
2020-02-11 02:36:43 +0500

    Update q2.c

--- q2.c
+++ q2.c
@@ -6,6 +6,9 @@ int main()
 	FILE *output;	
 	output = fopen("b.txt", "w");
 
-	
+	printf("%s\n", "Please select one of the options as (1 or 2 or 3)" );
+	printf("%s\n", "1) Add a new student" );
+	printf("%s\n", "2) Read student record ");
+	printf("%s\n", "3) Delete student record");
 	return 0;
 }
